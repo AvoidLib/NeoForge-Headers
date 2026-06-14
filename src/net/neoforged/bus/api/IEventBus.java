450bc5b4b907f5e06cd34ac29902e248da65374e
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-06-14 17:31:47 +0200

    IEventBus addListener generic fix

--- src/net/neoforged/bus/api/IEventBus.java
+++ src/net/neoforged/bus/api/IEventBus.java
@@ -3,5 +3,5 @@ package net.neoforged.bus.api;
 import java.util.function.Consumer;
 
 public abstract class IEventBus {
-    public abstract void addListener(Consumer<?> callback);
+    public abstract <T> void addListener(Consumer<T> callback);
 }
